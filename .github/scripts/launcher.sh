Infineon/optiga-tpm-cheatsheet

commit 8847bfd55a6c687f93120d7c73baaa49e247444b

CI Fix: CI will mostly fail on non-master branch (#5)
Script generated from README.md will always fetch from master branch. Hence, skip the "git clone" and duplicate the project from GITHUB_WORKSPACE (actions/checkout) before launching the docker.

Signed-off-by: wenxin.leong <wenxin.leong@infineon.com>

.github/scripts/launcher.sh
@@ -7,17 +7,21 @@ set -exo pipefail
 
 # GITHUB_WORKSPACE is set by actions/checkout@v3
 
-export DOCKER_BUILD_DIR="/$PROJECT_NAME"
+export DOCKER_WORKSPACE_DIR="~/${PROJECT_NAME}"
+export WORKSPACE_DIR="~/${PLATFORM}_${DOCKER_IMAGE}"
+
+# Do not share the same workspace
+cp -rf $GITHUB_WORKSPACE $WORKSPACE_DIR
 
 docker run \
            --memory-swap -1 \
            --platform `echo ${PLATFORM} | sed 's/-/\//'` \
-           --env WORKSPACE_DIR=$DOCKER_BUILD_DIR \
+           --env WORKSPACE_DIR=$DOCKER_WORKSPACE_DIR \
            --env DOCKER_IMAGE=$DOCKER_IMAGE \
            --env PLATFORM=$PLATFORM \
            --env-file .ci/docker.env \
-           -v "$GITHUB_WORKSPACE:$DOCKER_BUILD_DIR" \
+           -v "${WORKSPACE_DIR}:${DOCKER_WORKSPACE_DIR}" \
            `echo ${DOCKER_IMAGE} | sed 's/-/:/'` \
-           /bin/bash -c "$DOCKER_BUILD_DIR/.ci/docker.sh"
+           /bin/bash -c "${DOCKER_WORKSPACE_DIR}/.ci/docker.sh"
 
 exit 0

commit 67dc17b75030191eebd6a1d26c4ff12ec1899213

Fix CI bug introduced by commit 8847bfd
Signed-off-by: wenxin.leong <wenxin.leong@infineon.com>

.github/scripts/launcher.sh
@@ -7,21 +7,21 @@ set -exo pipefail
 
 # GITHUB_WORKSPACE is set by actions/checkout@v3
 
-export DOCKER_WORKSPACE_DIR="~/${PROJECT_NAME}"
-export WORKSPACE_DIR="~/${PLATFORM}_${DOCKER_IMAGE}"
+export DOCKER_WORKSPACE_DIR="/root/$PROJECT_NAME"
+export WORKSPACE_DIR=~/${PLATFORM}_${DOCKER_IMAGE}
 
 # Do not share the same workspace
 cp -rf $GITHUB_WORKSPACE $WORKSPACE_DIR
 
 docker run \
            --memory-swap -1 \
-           --platform `echo ${PLATFORM} | sed 's/-/\//'` \
+           --platform `echo $PLATFORM | sed 's/-/\//'` \
            --env WORKSPACE_DIR=$DOCKER_WORKSPACE_DIR \
            --env DOCKER_IMAGE=$DOCKER_IMAGE \
            --env PLATFORM=$PLATFORM \
            --env-file .ci/docker.env \
            -v "${WORKSPACE_DIR}:${DOCKER_WORKSPACE_DIR}" \
-           `echo ${DOCKER_IMAGE} | sed 's/-/:/'` \
+           `echo $DOCKER_IMAGE | sed 's/-/:/'` \
            /bin/bash -c "${DOCKER_WORKSPACE_DIR}/.ci/docker.sh"
 
 exit 0
67dc17b75030191eebd6a1d26c4ff12ec1899213
wenxin.leong <wenxin.leong@infineon.com>
2023-01-08 22:53:26 -0800

    Fix CI bug introduced by commit 8847bfd
    Signed-off-by: wenxin.leong <wenxin.leong@infineon.com>

--- .github/scripts/launcher.sh
+++ .github/scripts/launcher.sh
@@ -7,21 +7,21 @@ set -exo pipefail
 
 # GITHUB_WORKSPACE is set by actions/checkout@v3
 
-export DOCKER_WORKSPACE_DIR="~/${PROJECT_NAME}"
-export WORKSPACE_DIR="~/${PLATFORM}_${DOCKER_IMAGE}"
+export DOCKER_WORKSPACE_DIR="/root/$PROJECT_NAME"
+export WORKSPACE_DIR=~/${PLATFORM}_${DOCKER_IMAGE}
 
 # Do not share the same workspace
 cp -rf $GITHUB_WORKSPACE $WORKSPACE_DIR
 
 docker run \
            --memory-swap -1 \
-           --platform `echo ${PLATFORM} | sed 's/-/\//'` \
+           --platform `echo $PLATFORM | sed 's/-/\//'` \
            --env WORKSPACE_DIR=$DOCKER_WORKSPACE_DIR \
            --env DOCKER_IMAGE=$DOCKER_IMAGE \
            --env PLATFORM=$PLATFORM \
            --env-file .ci/docker.env \
            -v "${WORKSPACE_DIR}:${DOCKER_WORKSPACE_DIR}" \
-           `echo ${DOCKER_IMAGE} | sed 's/-/:/'` \
+           `echo $DOCKER_IMAGE | sed 's/-/:/'` \
            /bin/bash -c "${DOCKER_WORKSPACE_DIR}/.ci/docker.sh"
 
 exit 0
